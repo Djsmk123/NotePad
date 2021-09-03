8c73c9d8758462793deaf2064a6be8fe9accf406
Djsmk123 <74850678+Djsmk123@users.noreply.github.com>
2021-09-03 08:43:26 +0530

    Add files via upload

---
+++ lib/Views/SplashScreen.dart
@@ -0,0 +1,80 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:notepad/Views/AuthScreen.dart';
+import 'package:notepad/Views/HomeScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../Constants.dart';
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+ImageProvider logo = AssetImage("assets/Checklist.jpg");
+class _SplashScreenState extends State<SplashScreen> {
+  Future<void> userAuth()
+  async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    email = prefs.getString('email').toString();
+    password = prefs.getString('password').toString();
+    if (email != 'null' && password!='null') {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      Navigator.pop(context);
+      Navigator.pushNamed(context, HomeScreen.id);
+    }
+    else
+      {
+        Navigator.pop(context);
+        Navigator.pushNamed(context, AuthScreen.id);
+      }
+  }
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      userAuth();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+            "NotePad",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+              color: Color(0XFF17224b),
+            )
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+           child: Image(image: logo,),
+          ),
+          SizedBox(height: 30,),
+          SpinKitThreeBounce(
+            size: 30,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: index.isEven ? Colors.black : Colors.grey,
+                ),
+              );
+            },
+          ),
+
+        ],
+      ),
+    );
+  }
+}
